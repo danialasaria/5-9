ee696d6648781dea8ebaf65513666075f384721c
danial.asaria <dasaria@usc.edu>
2022-05-08 22:43:43 -0700

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080; // Step 1
 
 
 // Step 2
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/userDB', {
     useNewUrlParser: true
 });
 
