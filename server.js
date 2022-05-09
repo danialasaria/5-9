1cd7a8a34fd48d2061a1886b26ddea86763b473a
danial.asaria <dasaria@usc.edu>
2022-05-08 23:25:27 -0700

    testing

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 
 // Importing Modules
 const mongoose = require('mongoose');
@@ -13,10 +14,13 @@ const app = express();
 const log = console.log;
 const PORT = process.env.PORT || 8080; // Step 1
 
+console.log(process.env.MONGODB_URI)
 
 // Step 2
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/userDB', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 // Configuration
@@ -30,6 +34,10 @@ if (process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+} else {
+    app.get("*", (req, res) => {
+        res.send("Api running");
     });
 }
 
